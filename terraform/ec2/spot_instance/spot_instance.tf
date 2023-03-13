759c08a6d79300e6bf69d07ffc98fc9be6163096
Bill Burn <williamburn@gmail.com>
2023-03-13 18:45:29 -0500

    Updating spot code

--- terraform/ec2/spot_instance/spot_instance.tf
+++ terraform/ec2/spot_instance/spot_instance.tf
@@ -7,16 +7,22 @@ terraform {
   }
 }
 
-resource "aws_spot_instance_request" "sample_spot"" {
-  ami = "ami-005f9685cb30f234b"
-  availability_zone = "us-east-1a"
-  instance_type = "t3.small"
-  key_name = "devsecops-key-01"
-  security_groups = [ "ssh-from-home" ]
+resource "aws_spot_instance_request" "sample_spot_ec2" {
+  ami               = "ami-005f9685cb30f234b"
+  availability_zone = "us-east-1b"
+  instance_type     = "t3.small"
+  key_name          = "devsecops-key-01"
+  security_groups   = ["ssh-from-home"]
+  spot_price        = "0.0075"
+  spot_type         = "one-time"
 
   tags = {
-    Name            = "devsecops-spot-server-01"
-    Date            = "03-12-2023"
+    Name = "devsecops-spot-server-01"
+    Date = "03-13-2023"
   }
 
+}
+
+output "instance_ip_addr" {
+  value = aws_spot_instance_request.sample_spot_ec2.public_ip
 }
